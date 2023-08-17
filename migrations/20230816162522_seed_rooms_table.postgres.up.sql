591135c0facc725ca4ec3bf046150ddca449ca77
Jiawei Liu <yoshio@Jiaweis-MacBook-Pro.local>
2023-08-17 17:21:40 +0900

    finishing database

---
+++ migrations/20230816162522_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at)
+VALUES ('General''s Quarters', '2023-08-03 00:00:00.000', '2023-08-03 00:00:00.000'),
+       ('Major''s Suite', '2023-08-03 00:00:00.000', '2023-08-03 00:00:00.000');
